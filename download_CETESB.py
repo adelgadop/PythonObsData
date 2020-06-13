f89907022927bed17e33fa4892107c88b60ea26c
Alejandro Delgado <adelgado@iag.usp.br>
2020-06-12 23:49:13 -0500

    Updated

--- download_CETESB.py
+++ download_CETESB.py
@@ -4,8 +4,13 @@ import pandas as pd
 import qualar_py as qr
 
 cetesb_stations = pd.read_csv(
-    '../3_Validation/cetesb_station_2017_codes_qualr.csv', encoding = "ISO-8859-1")
-cetesb_stations[1:]
+    '../3_Validation/cetesb_station_2017_codes_qualr.csv', 
+    encoding = "ISO-8859-1")
+
+print(cetesb_stations)
+start = int(input('start station (number):'))
+cetesb_stations = cetesb_stations[start:]
+print(cetesb_stations)
 
 obs_dates = pd.DataFrame({'date': pd.date_range(input('date (YYYY-MM-DD):'), 
             periods=int(input('days:'))*24, freq='H')})
@@ -18,11 +23,9 @@ end_date = obs_dates.date_qualar.values[-1]
 
 print(obs_dates)
 
- for i in cetesb_stations.code:
-     print('Downloading poll ' + cetesb_stations.name[cetesb_stations.code == i].values + ' Station')
-     qr.all_photo(cetesb_login, cetesb_password,
-                         start_date, end_date, i, csv_photo=True)
-     print('Downloading meteo ' + cetesb_stations.name[cetesb_stations.code == i].values + ' Station')
-     qr.all_met(cetesb_login, cetesb_password,
-                         start_date, end_date, i, csv_met=True)
+for i in cetesb_stations.code:
+    print('Downloading poll ' + cetesb_stations.name[cetesb_stations.code == i].values + ' Station')
+    qr.all_photo(cetesb_login, cetesb_password, start_date, end_date, i, csv_photo=True)
+    print('Downloading meteo ' + cetesb_stations.name[cetesb_stations.code == i].values + ' Station')
+    qr.all_met(cetesb_login, cetesb_password, start_date, end_date, i, csv_met=True)
 
